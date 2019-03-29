38a448fbe2c641b83b0906ca421848c570e7d89a
reina <ReinaSHSL@users.noreply.github.com>
2019-03-29 03:36:54 -0700

    delete unnecessary

--- src/main/java/reinashsl/Replay.java
+++ src/main/java/reinashsl/Replay.java
@@ -5,11 +5,5 @@ public class Replay {
     String CHARACTER;
     String[] FLOORS;
     String[] FLOOR_CHOICES;
-    String[] MERCHANT_ACTIONS;
-    String[] TURNS;
-    String[] COMBAT_ACTIONS;
-    String[] EVENT_CHOICES;
-    String[] REWARD_CHOICES;
-    String[] BOSS_CHOICES;
     String[] NEOW;
 }
