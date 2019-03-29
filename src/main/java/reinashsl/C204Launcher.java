cfe542ccdb98e3cdabd8ced30aebd26a3005f43f
reina <ReinaSHSL@users.noreply.github.com>
2019-03-29 02:22:12 -0700

    load replay

--- src/main/java/reinashsl/C204Launcher.java
+++ src/main/java/reinashsl/C204Launcher.java
@@ -3,6 +3,14 @@ package reinashsl;
 import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.google.gson.JsonArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 @SpireInitializer
 public class C204Launcher implements
@@ -17,4 +25,16 @@ public class C204Launcher implements
 
     }
 
+    public static void loadReplay(File replay) {
+        JSONParser jsonParser = new JSONParser();
+        try {
+            FileReader reader = new FileReader(replay.getAbsoluteFile());
+            JsonArray a = (JsonArray) jsonParser.parse(reader);
+            for (Object o : a) {
+                System.out.println(o);
+            }
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/reinashsl/ui/ReplayButton.java
+++ src/main/java/reinashsl/ui/ReplayButton.java
@@ -1,10 +1,15 @@
 package reinashsl.ui;
 
+import com.evacipated.cardcrawl.modthespire.lib.ByRef;
 import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.evacipated.cardcrawl.modthespire.lib.SpireInsertPatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.megacrit.cardcrawl.screens.mainMenu.MainMenuScreen;
 import com.megacrit.cardcrawl.screens.mainMenu.MenuButton;
+import reinashsl.C204Launcher;
 
 import javax.swing.*;
+import java.io.File;
 import java.lang.reflect.Field;
 
 public class ReplayButton {
@@ -40,10 +45,38 @@ public class ReplayButton {
     {
         public static void Postfix(MenuButton __instance)
         {
-            if (__instance.result == REPLAYS) {
-                JFileChooser fc = new JFileChooser();
-                int returnVal = fc.showOpenDialog(null);
+            try {
+                if (__instance.result == REPLAYS) {
+                    JFileChooser fc = new JFileChooser();
+                    int returnVal = fc.showOpenDialog(null);
+                    if (returnVal == JFileChooser.APPROVE_OPTION) {
+                        File file = fc.getSelectedFile();
+                        Field f_label = MenuButton.class.getDeclaredField("label");
+                        f_label.setAccessible(true);
+                        f_label.set(__instance, "Load Replay [" + file.getName() + "]");
+                        C204Launcher.loadReplay(file);
+                    }
+                }
+            } catch (IllegalAccessException | NoSuchFieldException e) {
+                e.printStackTrace();
             }
+        }
+    }
+
+    @SpirePatch(
+            clz = MainMenuScreen.class,
+            method = "setMainMenuButtons"
+    )
+    public static class MainMenuItem
+    {
+        @SpireInsertPatch(
+                rloc=4,
+                localvars={"index"}
+        )
+        public static void Insert(Object __obj_instance, @ByRef int[] index)
+        {
+            MainMenuScreen __instance = (MainMenuScreen)__obj_instance;
+            __instance.buttons.add(new MenuButton(ReplayButton.REPLAYS, index[0]++));
         }
     }
 
