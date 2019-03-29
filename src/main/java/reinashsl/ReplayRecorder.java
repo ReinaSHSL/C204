aa479a951bd8ebf5498e7d6912578af021f55045
reina <ReinaSHSL@users.noreply.github.com>
2019-03-29 16:27:17 -0700

    Recorder

--- src/main/java/reinashsl/ReplayRecorder.java
+++ src/main/java/reinashsl/ReplayRecorder.java
@@ -1,5 +1,41 @@
 package reinashsl;
 
-public class ReplayRecorder {
+import basemod.BaseMod;
+import basemod.interfaces.OnCardUseSubscriber;
+import basemod.interfaces.PostDeathSubscriber;
+import basemod.interfaces.PostEnergyRechargeSubscriber;
+import basemod.interfaces.PostPotionUseSubscriber;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.potions.AbstractPotion;
 
+public class ReplayRecorder implements
+        PostPotionUseSubscriber,
+        PostEnergyRechargeSubscriber,
+        OnCardUseSubscriber,
+        PostDeathSubscriber
+{
+
+    public ReplayRecorder() {
+        BaseMod.subscribe(this);
+    }
+
+    @Override
+    public void receiveCardUsed(AbstractCard abstractCard) {
+
+    }
+
+    @Override
+    public void receivePostDeath() {
+
+    }
+
+    @Override
+    public void receivePostEnergyRecharge() {
+
+    }
+
+    @Override
+    public void receivePostPotionUse(AbstractPotion abstractPotion) {
+
+    }
 }
