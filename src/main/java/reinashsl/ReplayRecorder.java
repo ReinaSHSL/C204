96fa958df6d8812d0953dd21959970bb4872f8ed
reina <ReinaSHSL@users.noreply.github.com>
2019-03-29 17:39:21 -0700

    Write run to file

--- src/main/java/reinashsl/ReplayRecorder.java
+++ src/main/java/reinashsl/ReplayRecorder.java
@@ -2,11 +2,20 @@ package reinashsl;
 
 import basemod.BaseMod;
 import basemod.interfaces.*;
+import com.badlogic.gdx.Gdx;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.potions.AbstractPotion;
 import org.json.simple.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 public class ReplayRecorder implements
         PostPotionUseSubscriber,
@@ -28,7 +37,14 @@ public class ReplayRecorder implements
 
     @Override
     public void receivePostDeath() {
-
+        String path = FileSystems.getDefault().getPath(Gdx.files.getLocalStoragePath() + "replays" + File.separator + Settings.seed.toString() + ".shsl").toString();
+        try {
+            FileWriter fw = new FileWriter(path);
+            fw.write(currentRunReplay.toJSONString());
+            System.out.println("Replay saved");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
